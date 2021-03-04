f750318d051ed122b85a377b8213e8bc86c9945b
Matthew Casperson <matthewcasperson@gmail.com>
2021-03-05 09:51:30 +1000

    fixing the epub

--- scripts/create-k8s.sh
+++ scripts/create-k8s.sh
@@ -5,9 +5,11 @@ jekyll build --config config-k8s.yml
 
 pushd _site/k8s || exit
 find . -name '*.png' -exec mogrify -resize 50% {} +
-rm MyBook.epub
-echo "application/epub+zip" > mimetype
+if [ -f MyBook.epub ]; then
+    rm MyBook.epub
+fi
+echo -n "application/epub+zip" > mimetype
 zip -0Xq "MyBook.epub" "mimetype"
-zip -Xr9Dq "MyBook.epub" * -x "mimetype"
+zip -Xr9Dq "MyBook.epub" * -x "mimetype" -x "scripts/*"
 popd || exit
 popd || exit
