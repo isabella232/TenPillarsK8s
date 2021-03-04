c32dac6569a87d7fecfec374b942fbd11389ec22
Matthew Casperson <matthewcasperson@gmail.com>
2021-03-05 08:37:14 +1000

    first chapter draft done

---
+++ scripts/create-k8s.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pushd .. || exit
+jekyll build --config config-k8s.yml
+
+pushd _site/k8s || exit
+find . -name '*.png' -exec mogrify -resize 50% {} +
+rm MyBook.epub
+zip -Xr "MyBook.epub" "mimetype" *
+popd || exit
+popd || exit
