883a1c7e64b538782a1881f2f17c247c7671f9c6
Matthew Casperson <matthewcasperson@gmail.com>
2021-03-05 09:11:45 +1000

    fixed build

--- scripts/create-k8s.sh
+++ scripts/create-k8s.sh
@@ -6,6 +6,7 @@ jekyll build --config config-k8s.yml
 pushd _site/k8s || exit
 find . -name '*.png' -exec mogrify -resize 50% {} +
 rm MyBook.epub
+echo "application/epub+zip" > mimetype
 zip -Xr "MyBook.epub" "mimetype" *
 popd || exit
 popd || exit
