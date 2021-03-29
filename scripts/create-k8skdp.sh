3c4ee4a00425ff82b64e60dada06bf25401ade43
Matthew Casperson <matthewcasperson@gmail.com>
2021-03-29 11:44:20 +1000

    Build a printable epub

--- scripts/create-k8skdp.sh
+++ scripts/create-k8skdp.sh
@@ -4,6 +4,11 @@ pushd .. || exit
 jekyll build --config config-k8skdp.yml
 
 pushd _site/k8s || exit
+
+# Remove links in the book - this can't be printed anyway
+sed -i 's%<a href="[^"]*">%<u>%g' book.html
+sed -i 's%</a>%</u>%g' book.html
+
 # find . -name '*.png' -exec mogrify -resize 50% {} +
 if [ -f MyBook.epub ]; then
     rm MyBook.epub
